6b777f3b23d70e1094759d2974dfa614fc23676b
skonishi1125 <skonishi1125@gmail.com>
2022-11-22 21:16:57 +0900

    finish chapter4

--- original/chapter4/index.php
+++ original/chapter4/index.php
@@ -8,6 +8,31 @@
     default => 'うーん'
   };
   print $result;
-  
+
+  echo "<br><br>";
+
+  for ($i = 1, $j = 1; $result = $i * $j, $i < 6; $i++, $j++) {
+    print "{$i} * {$j} = {$result} <br>";
+  }
+
+  echo "<br><br>";
+
+  error_log('あわわ');
+?>
+
+<?php for ($i = 1; $i < 6; $i++) { ?>
+  <p>おはよー！</p>
+<?php }  ?>
+
+<?php
+
+  for ($i = 1; $i < 6; $i++) {
+    echo "わわわ<br>";
+    goto end;
+  }
+
+  end :
+  printf('end先です');
+
 
 ?>
